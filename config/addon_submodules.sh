5f71181fc1b1ebcc7c529469b062241aaf88db1d
George Avgeris <gavgeris@gmail.com>
2025-07-17 17:34:42 +0300

    Add Moodle plugins

---
+++ config/addon_submodules.sh
@@ -0,0 +1,70 @@
+#! /bin/sh
+
+# mods
+git submodule add https://github.com/learnweb/moodle-mod_groupmembers plugins/mod/groupmembers
+git submodule add https://github.com/PoetOS/moodle-mod_questionnaire plugins/mod/questionnaire
+git submodule add https://github.com/moodleworkplace/moodle-mod_coursecertificate plugins/mod/coursecertificate
+
+#blocks
+#git submodule add https://github.com/jonof/moodle-block_completion_progress blocks/completion_progress
+#git submodule add https://github.com/UniversityofPortland/moodle-block_custom_course_menu blocks/custom_course_menu
+#git submodule add https://github.com/remotelearner/moodle-block_grade_me blocks/grade_me
+#git submodule add https://github.com/Syxton/moodle-block_massaction blocks/massaction
+git submodule add https://github.com/donhinkelman/moodle-block_sharing_cart plugins/blocks/sharing_cart
+#git submodule add https://github.com/FMCorz/moodle-block_xp blocks/xp
+git submodule add https://github.com/jleyva/moodle-block_configurablereports/ plugins/blocks/configurable_reports
+
+
+#filters
+git submodule add https://github.com/michael-milette/moodle-filter_filtercodes plugins/filter/filtercodes
+git submodule add https://github.com/justinhunt/moodle-filter_generico plugins/filter/generico
+#git submodule add https://github.com/gavgeris/reusecontent filter/reusecontent
+#git submodule add https://github.com/Syxton/moodle-filter_sectionnames filter/sectionnames
+#git submodule add https://github.com/sharpchi/moodle-filter_syntaxhighlighter filter/syntaxhighlighter
+#git submodule add https://github.com/iarenaza/moodle-filter_multilang2 filter/multilang2
+
+#atto plugins
+#git submodule add https://github.com/johnhpercival/moodle-atto_bsgrid lib/editor/atto/plugins/bsgrid
+#git submodule add https://github.com/dthies/moodle-atto_fullscreen lib/editor/atto/plugins/fullscreen
+#git submodule add https://github.com/justinhunt/moodle-atto_generico lib/editor/atto/plugins/generico
+#git submodule add https://github.com/ndunand/moodle-atto_morebackcolors lib/editor/atto/plugins/morebackcolors
+#git submodule add https://github.com/moodle-an-hochschulen/moodle-atto_styles lib/editor/atto/plugins/styles
+#git submodule add https://github.com/sharpchi/moodle-atto_templates4u lib/editor/atto/plugins/templates4u
+#git submodule add https://github.com/rogersegu/moodle-atto_c4l/ lib/editor/atto/plugins/c4l
+#git submodule add https://github.com/ecampbell/moodle-atto_wordimport lib/editor/atto/plugins/wordimport
+
+#tinymce plugins
+#git submodule add https://github.com/reskit/moodle-tiny_c4l lib/editor/tiny/plugins/c4l
+
+
+#tools
+#git submodule add https://github.com/doiphode/moodle-tool_clearbackupfiles admin/tool/clearbackupfiles
+#git submodule add https://github.com/moodle-an-hochschulen/moodle-tool_redis admin/tool/redis
+git submodule add https://github.com/moodleworkplace/moodle-tool_certificate plugins/admin/tool/certificate
+
+#availability
+#git submodule add https://github.com/ewallah/moodle-availability_coursecompleted availability/condition/coursecompleted
+#git submodule add https://github.com/moodle-an-hochschulen/moodle-availability_role availability/condition/role
+
+#course formats
+#git submodule add https://github.com/cellule-tice/moodle-format_collapsibletopics course/format/collapsibletopics
+git submodule add https://github.com/gavgeris/moodle-format_remuiformat.git plugins/course/format/remuiformat
+#git submodule add https://github.com/WisdmLabs/moodle-format_remuiformat course/format/remuiformat
+#git submodule add https://github.com/gjb2048/moodle-format_topcoll.git course/format/topcoll
+#git submodule add https://github.com/marinaglancy/moodle-format_flexsections course/format/flexsections
+
+#themes
+#git submodule add https://gitlab.com/jezhops/moodle-theme_adaptable.git theme/adaptable
+#git submodule add https://github.com/dbnschools/moodle-theme_fordson theme/fordson
+#git submodule add https://github.com/willianmano/moodle-theme_moove theme/moove
+#git submodule add https://github.com/gavgeris/moodle-theme_etwinning theme/etwinning
+
+#locals
+#git submodule add https://github.com/moodle-an-hochschulen/moodle-local_boostnavigation local/boostnavigation
+git submodule add https://gitlab.com/adapta/moodle-local_modcustomfields plugins/local/modcustomfields
+git submodule add https://github.com/moodle-an-hochschulen/moodle-local_staticpage plugins/local/staticpage
+#git submodule add https://github.com/gavgeris/local_reusecontent_ws.git local/reusecontent_ws
+
+#enrolment
+git submodule add https://github.com/bobopinna/moodle-enrol_autoenrol plugins/enrol/autoenrol
+
