22d2a9971f8ff27fcb0eeface28f07016eeafdf5
George Avgeris <gavgeris@gmail.com>
2026-04-22 15:50:02 +0300

    make moodle.ini sample

--- config/docker-entrypoint.d/99-start-cron.sh
+++ config/docker-entrypoint.d/99-start-cron.sh
@@ -1,1 +1,5 @@
+# Dump current env vars to file (available at container start)
+printenv | grep -E 'MOODLE|MARIADB' > /etc/environment.cron
+chmod 600 /etc/environment.cron
+
 service cron start
--- scripts/init-moodle.sh
+++ scripts/init-moodle.sh
@@ -3,6 +3,6 @@ docker exec -u www-data moodle php /var/www/html/admin/cli/install_database.php 
   --fullname="My Moodle Site" \
   --shortname="moodle" \
   --adminuser=admin \
-  --adminpass=pass \
+  --adminpass=admin \
   --adminemail=gavgeris@gmail.com \
   --agree-license
